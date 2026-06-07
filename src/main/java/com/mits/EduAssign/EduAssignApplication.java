6196ba76d5e7d0bb8b197f30cde165d0961ffa94
BhavyaSree <mummadibhavyasree01@gmail.com>
2026-06-08 00:23:06 +0530

    first commit

---
+++ src/main/java/com/mits/EduAssign/EduAssignApplication.java
@@ -0,0 +1,13 @@
+package com.mits.EduAssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAssignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAssignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mits/EduAssign/EduAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mits.EduAssign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAssignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
